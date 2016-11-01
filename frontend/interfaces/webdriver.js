5137206aa86446d4ae798b9ec69f5dceabdd3064
Tom Robinson <tlrobinson@gmail.com>
2016-11-01 14:11:31 -0700

    Add a e2e test for settings, use nice webchauffeur webdriver wrapper [ci e2e]

--- frontend/interfaces/webdriver.js
+++ frontend/interfaces/webdriver.js
@@ -2,14 +2,20 @@
 declare module "selenium-webdriver" {
 
     declare class WebDriver {
+        getCurrentUrl(url: string): Promise<WebDriver>;
         wait(condition: Condition|Function, timeout: ?number): Promise<WebElement>;
         findElement(selector: By): WebElement;
+        deleteAllCookies(): Promise<void>;
+        getCurrentUrl(): Promise<string>;
     }
 
     declare class WebElement {
         click(): Promise<void>;
         findElement(selector: By): WebElement;
         getText(): Promise<string>;
+        sendKeys(keys: string): Promise<void>;
+        clear(): Promise<void>;
+        getAttribute(attribute: string): Promise<string>;
     }
 
     declare class Condition {
